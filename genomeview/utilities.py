bb22f75a07beb39782d4ff26b6b2700392dc451e
Noah Spies <nspies@stanford.edu>
2017-12-19 11:55:19 -0800

    adding utilities module

---
+++ genomeview/utilities.py
@@ -0,0 +1,11 @@
+def match_chrom_format(chrom, keys):
+    if chrom in keys:
+        return chrom
+    if "chr" in chrom:
+        chrom2 = chrom.replace("chr", "")
+    else:
+        chrom2 = "chr{}".format(chrom)
+        
+    if chrom2 in keys:
+        return chrom2
+    return chrom
