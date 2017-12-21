455b124949a595b6c7c06fd4d454d730006b26d7
Noah Spies <nspies@stanford.edu>
2017-12-21 15:33:13 -0800

    mocking some c-extension dependencies for docs

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy', 'pysam']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
