86559c1e2ec6871f4a6e15c9a9feca7de3cbcc47
Noah Spies <nspies@stanford.edu>
2018-01-22 09:54:51 -0800

    update tests

--- test/test_bed.py
+++ test/test_bed.py
@@ -2,6 +2,6 @@ import genomeview
 
 def test_bed():
     file_paths = ["genes.sorted.bed.gz"]
-    
-    doc = genomeview.visualize_data(file_paths, "chr3", 172277030, 180005230)
+
+    doc = genomeview.visualize_data(file_paths, "chr3", 179500230, 179800230)
     genomeview.save(doc, "bed_view.svg")
